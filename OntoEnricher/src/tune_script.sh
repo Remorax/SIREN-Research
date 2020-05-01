490a3a09593ff10e188554304d2548eaca40e5c8
Vivek Iyer <vivekbalasundaram@gmail.com>
2020-05-02 01:13:57 +0530

    Batch script for ada

--- OntoEnricher/src/tune_script.sh
+++ OntoEnricher/src/tune_script.sh
@@ -1,4 +1,18 @@
 
+
+#!/bin/bash
+
+#SBATCH -A research
+#SBATCH -n 10
+#SBATCH --gres=gpu:4
+#SBATCH --mem-per-cpu=10240
+#SBATCH --time=4-00:00:00
+#SBATCH --mail-type=END
+
+module load cuda/10.2
+module load cudnn/7.6.5-cuda-10.2 
+
+
 declare -a hidden_dim=(60 90 120) # Different frequencies considered while creating frequent paths
 declare -a layers=(2 4 6) # Maximum path lengths considered while extracting paths
 declare -a dropouts=(0.3 0.5 0.8) # Maximum path lengths considered while extracting paths
