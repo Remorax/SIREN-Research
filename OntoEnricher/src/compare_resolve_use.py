9dc57428753de41eb791933d968c18a875d873dc
Remorax <vivekbalasundaram@gmail.com>
2020-05-22 12:14:03 -0700

    Pushing compare use final version

--- OntoEnricher/src/compare_resolve_use.py
+++ OntoEnricher/src/compare_resolve_use.py
@@ -3,19 +3,21 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import numpy as np
 
+USE_folder = "/home/vlead/USE"
+
 f = open("../junk/failed_words", "rb")
 failed, _ = pickle.load(f)
 results = {i: ("", -1000) for i in failed}
 
-def compare_sim(word, embeds):
+def compare(wd, word, embeds):
     max_sim = -1000
     closest_word = ""
-    for i,w in enumerate(embeds):
-        sim = np.dot(word, w)
+    for w in embeds:
+        sim = np.dot(word, w[1])
         if sim > max_sim:
             max_sim = sim
-            closest_word = words[i]
-    print ("Original word: ", word, "Closest Word: ", closest_word, "Sim: ", max_sim)
+            closest_word = w[0]
+    print ("Original word: ", wd, "Closest Word: ", closest_word, "Sim: ", max_sim)
     return (closest_word, max_sim)
 
 def extractUSEEmbeddings(words):
@@ -25,12 +27,14 @@ def extractUSEEmbeddings(words):
 
 failed_embeds = extractUSEEmbeddings(failed)
 
+print ("Extracted failed embeddings")
 
 for file in ["../junk/" + s for s in os.listdir("../junk/") if s.startswith("use_embeddings_")]:
     emb_file = open(file, "rb")
     use_embeds = pickle.load(emb_file)
-    output = {word: compare(failed_embeds[i], use_embeds) for i,word in enumerate(failed)}
+    output = {word: compare(word, failed_embeds[i], use_embeds) for i,word in enumerate(failed)}
     results = {word: results[i] if results[i][1] > output[i][1] else output[i] for i in results}
+    print ("Parsing done for", file)
 
 resolved_file = open("../junk/resolved_use.pkl", "wb")
 pickle.dump(results, resolved_file)
