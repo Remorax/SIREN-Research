da4408fb96a1c7be3038b74bbf8b722983f06d3b
Vivek Iyer <vivekbalasundaram@gmail.com>
2020-05-23 00:51:06 +0530

    Pushing multiprocessing version

--- OntoEnricher/src/compare_resolve_use.py
+++ OntoEnricher/src/compare_resolve_use.py
@@ -18,23 +18,31 @@ def compare(wd, word, embeds):
             max_sim = sim
             closest_word = w[0]
     print ("Original word: ", wd, "Closest Word: ", closest_word, "Sim: ", max_sim)
-    return (closest_word, max_sim)
+    return (wd, closest_word, max_sim)
 
 def extractUSEEmbeddings(words):
     embed = hub.KerasLayer(USE_folder)
     word_embeddings = embed(words)
     return word_embeddings.numpy()
 
-failed_embeds = extractUSEEmbeddings(failed)
+def run():
+    failed_embeds = extractUSEEmbeddings(failed)
 
-print ("Extracted failed embeddings")
+    print ("Extracted failed embeddings")
 
-for file in ["../junk/" + s for s in os.listdir("../junk/") if s.startswith("use_embeddings_")]:
-    emb_file = open(file, "rb")
-    use_embeds = pickle.load(emb_file)
-    output = {word: compare(word, failed_embeds[i], use_embeds) for i,word in enumerate(failed)}
-    results = {word: results[i] if results[i][1] > output[i][1] else output[i] for i in results}
-    print ("Parsing done for", file)
+    for file in ["../junk/" + s for s in os.listdir("../junk/") if s.startswith("use_embeddings_")]:
+        emb_file = open(file, "rb")
+        use_embeds = pickle.load(emb_file)
+        output = {}
+        args = [(word, failed_embeds[i], use_embeds) for i,word in enumerate(failed)]
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            for result in executor.map(compare, args):
+                output[result[0]] = (result[1], result[2])
+        results = {word: results[i] if results[i][1] > output[i][1] else output[i] for i in results}
+        print ("Parsing done for", file)
 
-resolved_file = open("../junk/resolved_use.pkl", "wb")
-pickle.dump(results, resolved_file)
+    resolved_file = open("../junk/resolved_use.pkl", "wb")
+    pickle.dump(results, resolved_file)
+
+if __name__ == '__main__':
+    run()
