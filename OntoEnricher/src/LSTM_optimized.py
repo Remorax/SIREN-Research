7adc234fffca6f15677d22497fe96b8a9581121c
Vivek Iyer <vivekbalasundaram@gmail.com>
2020-10-02 01:37:02 +0530

    LSTM optimized script debugged on CPU fully; adds corrected instances and knocked files

--- OntoEnricher/src/LSTM_optimized.py
+++ OntoEnricher/src/LSTM_optimized.py
@@ -1,283 +1,240 @@
-import bcolz, pickle, torch, os, shelve, sys
-import concurrent.futures
+import pickle, torch, os, sys, random
 import numpy as np
 from math import ceil
-from itertools import count
-from collections import defaultdict
-from difflib import SequenceMatcher
-import tensorflow as tf
-import tensorflow_hub as hub
-from scipy import spatial
 import torch.optim as optim
 import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence
 from sklearn.metrics import accuracy_score
 
-relations = ["hypernym", "hyponym", "concept", "instance", "none"]
-NUM_RELATIONS = len(relations)
+dataset_file = os.path.abspath("../Input/" + sys.argv[1])
+results_file = os.path.abspath("../Results/" + sys.argv[2])
+model_file =  os.path.abspath("../Models/" + sys.argv[3])
 
-mappingDict = {key: idx for (idx,key) in enumerate(relations)}
-mappingDict_inv = {idx: key for (idx,key) in enumerate(relations)}
+f = open(dataset_file, "rb")
+(nodes_train, paths_train, counts_train, targets_train, 
+ nodes_test, paths_test, counts_test, targets_test,
+ nodes_instances, paths_instances, counts_instances, targets_instances,
+ nodes_knocked, paths_knocked, counts_knocked, targets_knocked,
+ emb_indexer, emb_indexer_inv, emb_vals, 
+ pos_indexer, dep_indexer, dir_indexer, rel_indexer) = pickle.load(f)
 
-dataset_file = os.path.abspath("../junk/Input/" + sys.argv[1])
-results_file = os.path.abspath("../junk/Results/" + sys.argv[2])
-model_file =  os.path.abspath("../junk/Models/" + sys.argv[3])
-        
+op_file = open(results_file, "w+")
+
 def write(statement):
-    op_file = open(results_file, "w+")
+    global op_file
     op_file.write("\n" + str(statement) + "\n")
-    op_file.close()
-
-def load_checkpoint(model, optimizer, filename=model_filename):
-    # Note: Input model & optimizer should be pre-defined.  This routine only updates their states.
-    start_epoch = 0
-    if os.path.isfile(filename):
-        write("=> loading checkpoint '{}'".format(filename))
-        checkpoint = torch.load(filename)
-        start_epoch = checkpoint['epoch']
-        model.load_state_dict(checkpoint['state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer'])
-        write("=> loaded checkpoint '{}' (epoch {})".format(filename, checkpoint['epoch']))
-    else:
-        write("=> no checkpoint found at '{}'".format(filename))
-
-    return model, optimizer, start_epoch
-
 
 POS_DIM = 4
-DEP_DIM = 5
-DIR_DIM = 1
-EMBEDDING_DIM = 512
-NULL_PATH = ((0, 0, 0, 0),)
-
-
-file = open(dataset_file, 'rb')
-parsed_train, parsed_test, parsed_instances, parsed_knocked, pos_indexer, dep_indexer, dir_indexer  = pickle.load(file)
-
-relations = ["hypernym", "hyponym", "concept", "instance", "none"]
-NUM_RELATIONS = len(relations)
+DEP_DIM = 6
+DIR_DIM = 3
+NUM_RELATIONS = len(rel_indexer)
+NULL_EDGE = [0, 0, 0, 0]
 
 torch.set_default_dtype(torch.float64)
 torch.manual_seed(0)
 np.random.seed(0)
+random.seed(0)
 
-class LSTM(nn.Module):
+flatten = lambda l: [item for sublist in l for item in sublist]
 
-    def __init__(self):
+class RelationPredictor(nn.Module):
+
+    def __init__(self, emb_vals):
         
-        super(LSTM, self).__init__()
-        self.cache = {}
+        super(RelationPredictor, self).__init__()
+
+        self.EMBEDDING_DIM = np.array(emb_vals).shape[1]
+        self.n_directions = 2 if bidirectional else 1
         
-        self.hidden_dim = 2 * HIDDEN_DIM + 2 * EMBEDDING_DIM
-        self.input_dim = POS_DIM + DEP_DIM + EMBEDDING_DIM + DIR_DIM
-        self.layer1_dim = LAYER1_DIM
+        self.input_dim = POS_DIM + DEP_DIM + self.EMBEDDING_DIM + DIR_DIM
+        self.output_dim = self.n_directions * HIDDEN_DIM + 2 * self.EMBEDDING_DIM
+        # self.layer1_dim = LAYER1_DIM
         # self.W1 = nn.Linear(self.hidden_dim, self.layer1_dim)
         # self.W2 = nn.Linear(self.layer1_dim, NUM_RELATIONS)
-        self.W = nn.Linear(self.hidden_dim, NUM_RELATIONS)
 
         self.dropout_layer = nn.Dropout(p=dropout)
-        self.softmax = nn.LogSoftmax()
+        self.log_softmax = nn.LogSoftmax()
         
+        self.name_embeddings = nn.Embedding(len(emb_vals), self.EMBEDDING_DIM)
+        self.name_embeddings.load_state_dict({'weight': torch.from_numpy(np.array(emb_vals))})
+        self.name_embeddings.weight.requires_grad = False
+
         self.pos_embeddings = nn.Embedding(len(pos_indexer), POS_DIM)
         self.dep_embeddings = nn.Embedding(len(dep_indexer), DEP_DIM)
         self.dir_embeddings = nn.Embedding(len(dir_indexer), DIR_DIM)
-        
+
         nn.init.xavier_uniform_(self.pos_embeddings.weight)
         nn.init.xavier_uniform_(self.dep_embeddings.weight)
         nn.init.xavier_uniform_(self.dir_embeddings.weight)
-
-        self.lstm = nn.LSTM(self.input_dim, HIDDEN_DIM, NUM_LAYERS, bidirectional=True)
-    
-    def normalize_embeddings(self, embeds):
         
-        embed = torch.flatten(self.dropout_layer(embeds))
-        return embed
+        self.lstm = nn.LSTM(self.input_dim, HIDDEN_DIM, NUM_LAYERS, bidirectional=bidirectional, batch_first=True)
 
-    def embed_path(self, elem):
-        path, count = elem
-        if path in self.cache:
-            return self.cache[path] * count
-        lstm_inp = torch.Tensor([]).to(device)
-        for edge in path:
-            word_embed = self.normalize_embeddings(edge[0])
-            pos_embed = self.normalize_embeddings(self.pos_embeddings(edge[1].long()))
-            dep_embed = self.normalize_embeddings(self.dep_embeddings(edge[2].long()))
-            dir_embed = self.normalize_embeddings(self.dir_embeddings(edge[3].long()))
-            # print (word_embed.shape, pos_embed.shape, dep_embed.shape, dir_embed.shape)
-            embeds = torch.cat((word_embed, pos_embed, dep_embed, dir_embed)).view(1, -1)
-            lstm_inp = torch.cat((lstm_inp, embeds), 0)
+        self.W = nn.Linear(self.output_dim, NUM_RELATIONS)
 
-        lstm_inp = lstm_inp.view(-1, 1, self.input_dim)
-        output, _ = self.lstm(lstm_inp)
-        output = output[-1]
-        self.cache[path] = output
-        return output * count
-    
-    def forward(self, data, emb_indexer):
+    def forward(self, nodes, paths, counts, edgecounts, max_paths, max_edges):
+        '''
+            nodes: batch_size * 2
+            paths: batch_size * max_paths * max_edges * 4
+            counts: batch_size * max_paths
+            edgecounts: batch_size * max_paths
+        '''
+        word_embed = self.dropout_layer(self.name_embeddings(paths[:,:,:,0]))
+        pos_embed = self.dropout_layer(self.pos_embeddings(paths[:,:,:,1]))
+        dep_embed = self.dropout_layer(self.dep_embeddings(paths[:,:,:,2]))
+        dir_embed = self.dropout_layer(self.dir_embeddings(paths[:,:,:,3]))
+        paths_embed = torch.cat((word_embed, pos_embed, dep_embed, dir_embed), dim=-1)
+        nodes_embed = self.dropout_layer(self.name_embeddings(nodes)).reshape(-1, 2*self.EMBEDDING_DIM)
+
+        paths_embed = paths_embed.reshape((-1, max_edges, self.input_dim))
+
+        paths_packed = pack_padded_sequence(paths_embed, torch.flatten(edgecounts), batch_first=True, enforce_sorted=False)
+        _, (hidden_state, _) = self.lstm(paths_packed)
+        paths_output = hidden_state.permute(1,2,0)
+        paths_output_reshaped = paths_output.reshape(-1, max_paths, HIDDEN_DIM*NUM_LAYERS*self.n_directions)
+        # paths_output has dim (batch_size, max_paths, HIDDEN_DIM, NUM_LAYERS*self.n_directions)
+
+        paths_weighted = torch.bmm(paths_output_reshaped.permute(0,2,1), counts.unsqueeze(-1)).squeeze(-1)
+        representation = torch.cat((nodes_embed, paths_weighted), dim=-1)
+        probabilities = self.log_softmax(self.W(representation))
+        return probabilities
+
+def to_list(seq):
+    for item in seq:
+        if isinstance(item, tuple):
+            yield list(to_list(item))
+        elif isinstance(item, list):
+            yield [list(to_list(elem)) for elem in item]
+        else:
+            yield item
+
+def pad_paths(paths, max_paths, max_edges):
+    paths_edgepadded = [[path + [NULL_EDGE for i in range(max_edges-len(path))]
+        for path in element]
+    for element in paths]
+    NULL_PATH = [NULL_EDGE for i in range(max_edges)]
+    paths_padded = [element + [NULL_PATH for i in range(max_paths-len(element))] 
+        for element in paths_edgepadded]
+    return np.array(paths_padded)
         
-        # print ("Data: ", data.shape, emb_indexer.shape)
-        h = torch.Tensor([]).to(device)
-        idx = 0
-        for paths in data:
-            paths_embeds = torch.Tensor([]).to(device)
-            for path in paths.items():
-                emb = self.embed_path(path).view(1,-1)
-                paths_embeds = torch.cat((paths_embeds, emb), 0)
-            path_embedding = torch.div(torch.sum(paths_embeds, 0), sum(list(paths.values())))
-            # print (emb_indexer[idx][0].shape, emb_indexer[idx][1].shape, emb_indexer[idx])
-            x = torch.DoubleTensor([[emb_indexer[idx][0]]]).to(device).view(EMBEDDING_DIM)
-            y = torch.DoubleTensor([[emb_indexer[idx][1]]]).to(device).view(EMBEDDING_DIM)
-            path_embedding_cat = torch.cat((x, path_embedding, y))
-            # print ("Path embedding after cat with embeddings: ", path_embedding.shape)
-            # layer1_output  = self.softmax(self.W1(path_embedding_cat))
-            probabilities = self.softmax(self.W(path_embedding_cat))
-            # print ("Probabilities: ", probabilities)
-            h = torch.cat((h, probabilities.view(1,-1)), 0)
-            idx += 1
-         
-        return h
+def pad_counts(counts, max_paths):
+    return np.array([elem + [0 for i in range(max_paths - len(elem))] for elem in counts])
 
-def log_loss(output, target):
-    prob_losses = torch.Tensor([]).double().to(device)
-    for i,batch in enumerate(output):
-        # print (i, batch)
-        log_prob = -1 * torch.log(batch[target[i]])
-        # print (log_prob, log_prob.shape)
-        prob_losses = torch.cat((prob_losses, torch.unsqueeze(log_prob, 0)), 0)
-    # print (prob_losses, prob_losses.shape)
-    return torch.sum(prob_losses)
+def pad_edgecounts(edgecounts, max_paths):
+    return np.array([elem + [1 for i in range(max_paths - len(elem))] for elem in edgecounts])
 
-def tensorifyTuple(tup):
-    return tuple([tuple([torch.DoubleTensor([[e]]).to(device) for e in edge]) for edge in tup])
-    
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-
-# print ("num_relations:", NUM_RELATIONS)
 HIDDEN_DIM = 250
-LAYER1_DIM = 120
-NUM_LAYERS = 3
+# LAYER1_DIM = 120
+NUM_LAYERS = 1
 num_epochs = 50
-batch_size = 5000
+batch_size = 32
+bidirectional = True
 
-dataset_size = len(parsed_train[2])
-batch_size = min(batch_size, dataset_size)
-num_batches = int(ceil(dataset_size/batch_size))
-
-lr = 0.01
+lr = 0.001
 dropout = 0.3
-lstm = nn.DataParallel(LSTM()).to(device)
+weight_decay = 0.001
+
+model = nn.DataParallel(RelationPredictor(emb_vals)).to(device)
 criterion = nn.NLLLoss()
-optimizer = optim.AdamW(lstm.parameters(), lr=lr)
+optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
 
-                   
-loss_list = []
-
-epochs_range = range(prev_epoch, num_epochs) if prev_epoch!=-1 else range(num_epochs)
-
-try:
-    for epoch in epochs_range:
+for epoch in range(num_epochs):
     
-        total_loss, epoch_idx = 0, np.random.permutation(dataset_size)
-        
-        if prev_epoch!=-1 and epoch==prev_epoch:
-            lstm, optimizer, curr_epoch = load_checkpoint(lstm, optimizer)
-            lstm = lstm.to(device)
-            for state in optimizer.state.values():
-                for k, v in state.items():
-                    if isinstance(v, torch.Tensor):
-                        state[k] = v.to(device)
-                        
-        for batch_idx in range(num_batches):
-            
-            write("Batch_idx " + str(batch_idx))
-            batch_end = (batch_idx+1) * batch_size
-            batch_start = batch_idx * batch_size
-            batch = epoch_idx[batch_start:batch_end]
-            
-            # print ("x_train", x_train[batch], "emb", embed_indices_train[batch])
-            data = [{tensorifyTuple(e): dictElem[e] for e in dictElem} for dictElem in np.array(parsed_train[1])[batch]]
-            labels, embeddings_idx = np.array(parsed_train[2])[batch], np.array(parsed_train[0])[batch]
-            
-            # Run the forward pass
-            outputs = lstm(data, embeddings_idx)
-            # print (outputs, labels)
-            #loss = log_loss(outputs, torch.LongTensor(labels).to(device))
-            loss = criterion(outputs, torch.LongTensor(labels).to(device))
-            
-            write("Loss: " + str(loss.item()))
-            # Backprop and perform Adam optimisation
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item()
-        state = {'epoch': epoch + 1, 'state_dict': lstm.state_dict(),
-                 'optimizer': optimizer.state_dict()}
-        torch.save(state, model_filename)
-        
-        total_loss /= dataset_size
-        write('Epoch [{}/{}] Loss: {:.4f}'.format(epoch + 1, num_epochs, total_loss))
-        loss_list.append(loss.item())
+    all_inp = list(zip(nodes_train, paths_train, counts_train, targets_train))
+    all_inp_shuffled = random.sample(all_inp, len(all_inp))
+    nodes_train, paths_train, counts_train, targets_train = list(zip(*all_inp_shuffled))
 
-except Exception as e:
-    print (e)
-    sys.exit(epoch)
+    num_edges_all = [[len(path) for path in element] for element in paths_train]
+    max_edges = max(flatten(num_edges_all))
+    max_paths = max([len(elem) for elem in counts_train])
+
+    dataset_size = len(nodes_train)
+    batch_size = min(batch_size, dataset_size)
+    num_batches = int(ceil(dataset_size/batch_size))
+
+    for batch_idx in range(num_batches):
+        
+        batch_start = batch_idx * batch_size
+        batch_end = (batch_idx+1) * batch_size
+        
+        nodes = torch.LongTensor(nodes_train[batch_start:batch_end]).to(device)
+        paths = torch.LongTensor(pad_paths(paths_train[batch_start:batch_end], max_paths, max_edges)).to(device)
+        counts = torch.DoubleTensor(pad_counts(counts_train[batch_start:batch_end], max_paths)).to(device)
+        edgecounts = torch.LongTensor(pad_edgecounts(num_edges_all[batch_start:batch_end], max_paths)).to(device)
+        targets = torch.LongTensor(targets_train[batch_start:batch_end]).to(device)
+        
+        # Backprop and perform Adam optimisation
+        optimizer.zero_grad()
+
+        # Run the forward pass
+        outputs = model(nodes, paths, counts, edgecounts, max_paths, max_edges)
+
+        #loss = log_loss(outputs, torch.LongTensor(labels).to(device))
+        loss = criterion(outputs, targets)
+
+        loss.backward()
+        optimizer.step()
+
+        write("Epoch: {} Idx: {} Loss: {}".format(epoch, batch_idx, loss.item()))  
+
+write("Training Complete!")
 
 def calculate_recall(true, pred):
     true_f, pred_f = [], []
-    for i,l in enumerate(true):
-        if l!=4:
-            true_f.append(l)
+    for i,elem in enumerate(true):
+        if elem!=4:
+            true_f.append(elem)
             pred_f.append(pred[i])
     return accuracy_score(true_f, pred_f)
 
 def calculate_precision(true, pred):
     true_f, pred_f = [], []
-    for i,l in enumerate(pred):
-        if l!=4:
-            pred_f.append(l)
+    for i,elem in enumerate(pred):
+        if elem!=4:
+            pred_f.append(elem)
             true_f.append(true[i])
     return accuracy_score(true_f, pred_f)
 
-def test(test_dataset, message, output_file):
+def test(nodes_test, paths_test, counts_test, targets_test, message):
     predictedLabels, trueLabels = [], []
-    results = []
-    global mappingDict, mappingDict_inv
-    dataset_size = len(test_dataset[2])
-    batch_size = min(5000, dataset_size)
+
+    num_edges_all = [[len(path) for path in element] for element in paths_test]
+    max_edges = max(flatten(num_edges_all))
+    max_paths = max([len(elem) for elem in counts_test])
+
+    dataset_size = len(nodes_test)
+    batch_size = min(32, dataset_size)
     num_batches = int(ceil(dataset_size/batch_size))
 
-    test_perm = np.random.permutation(dataset_size)
     for batch_idx in range(num_batches):
         
-        batch_end = (batch_idx+1) * batch_size
         batch_start = batch_idx * batch_size
-        batch = test_perm[batch_start:batch_end]
+        batch_end = (batch_idx+1) * batch_size
 
-        data = [{tensorifyTuple(e): dictElem[e] for e in dictElem} for dictElem in np.array(test_dataset[1])[batch]]
-        labels, embeddings_idx = np.array(test_dataset[2])[batch], np.array(test_dataset[0])[batch]
-
-        outputs = lstm(data, embeddings_idx)
+        nodes = torch.LongTensor(nodes_test[batch_start:batch_end])
+        paths = torch.LongTensor(pad_paths(paths_test[batch_start:batch_end], max_paths, max_edges))
+        counts = torch.DoubleTensor(pad_counts(counts_test[batch_start:batch_end], max_paths))
+        edgecounts = torch.LongTensor(pad_edgecounts(num_edges_all[batch_start:batch_end], max_paths))
+        targets = torch.LongTensor(targets_test[batch_start:batch_end])
+        
+        outputs = model(nodes, paths, counts, edgecounts, max_paths, max_edges)
         _, predicted = torch.max(outputs, 1)
         predicted = [el.item() for el in predicted]
-        labels = [el.item() for el in labels]
+        targets = [el.item() for el in targets]
         predictedLabels.extend(predicted)
-        trueLabels.extend(labels)
-        results.extend(["\t".join(tup) for tup in zip(["\t".join(l) for l in np.array(test_dataset[3])[batch]], [mappingDict_inv[l] for l in predicted], [mappingDict_inv[l] for l in labels])])
-    open(output_file, "w+").write("\n".join(results))
-    print ("\n\n{}\n\n".format(message))
+        trueLabels.extend(targets)
     
-    if output_file!="test_knocked.tsv":
-        accuracy = accuracy_score(trueLabels, predictedLabels)
-        recall = calculate_recall(trueLabels, predictedLabels)
-        precision = calculate_precision(trueLabels, predictedLabels)
-        write ("Accuracy:" + str(accuracy) + str( " Precision:") + str(precision) + str(" Recall: ") + str(recall) + str("F1-score: ") + str(2 * (precision * recall/(precision + recall))))
-    else:
-        write ("Knocked:" + len([l for l in predictedLabels if l!=4]))
+    accuracy = accuracy_score(trueLabels, predictedLabels)
+    recall = calculate_recall(trueLabels, predictedLabels)
+    precision = calculate_precision(trueLabels, predictedLabels)
+    final_metrics = [accuracy, precision, recall, 2 * (precision * recall/(precision + recall))]
+    write("Final Results ({}): [{}]".format(message, ", ".join([str(el) for el in final_metrics])))
 
-lstm.eval()
+model.eval()
 with torch.no_grad():
-    test(parsed_test, "Test Set:", output_folder + "test.tsv")
-    test(parsed_instances, "Instance Set:", output_folder + "test_instance.tsv")
-    test(parsed_knocked, "Knocked out Set:", output_folder + "test_knocked.tsv")
+    test(nodes_test, paths_test, counts_test, targets_test, "Test")
+    test(nodes_instances, paths_instances, counts_instances, targets_instances, "Instances")
+    test(nodes_knocked, paths_knocked, counts_knocked, targets_knocked, "Knocked out")
 
-sys.exit(0)
+op_file.close()
