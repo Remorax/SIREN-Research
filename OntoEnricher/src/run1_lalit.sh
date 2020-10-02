cd259b476a484d8ccaa0f92ea4b2d16ad011acd3
Vivek Iyer <vivekbalasundaram@gmail.com>
2020-10-02 22:05:25 +0530

    pushing run scripts

---
+++ OntoEnricher/src/run1_lalit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -A research
+#SBATCH -n 10
+#SBATCH --gres=gpu:1
+#SBATCH --mem-per-cpu=10240
+#SBATCH --time=4-00:00:00
+#SBATCH --mail-type=END
+
+module load cuda/10.2
+module load cudnn/7.6.5-cuda-10.2 
+
+thresholds=(0.82 0.83 0.84 0.85 0.86)
+
+for threshold in "${thresholds[@]}";
+do
+	python3 LSTM_optimized.py "data_use_"$threshold".pkl" "results_threshold_"$threshold".txt" "Output_threshold_"$threshold "threshold_"$threshold".pt"
+done
---
+++ OntoEnricher/src/run2_vivek.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -A research
+#SBATCH -n 10
+#SBATCH --gres=gpu:1
+#SBATCH --mem-per-cpu=10240
+#SBATCH --time=4-00:00:00
+#SBATCH --mail-type=END
+
+module load cuda/10.2
+module load cudnn/7.6.5-cuda-10.2 
+
+thresholds=(0.67 0.68 0.69 0.7 0.71)
+
+for threshold in "${thresholds[@]}";
+do
+	python3 LSTM_optimized.py "data_use_"$threshold".pkl" "results_threshold_"$threshold".txt" "Output_threshold_"$threshold "threshold_"$threshold".pt"
+done
