9798d23d8a756d639d449a92ed07d7e6dca9e724
Vivek Iyer <vivekbalasundaram@gmail.com>
2020-10-03 15:28:20 +0530

    import nn.functional

--- OntoEnricher/src/LSTM_output_dropout.py
+++ OntoEnricher/src/LSTM_output_dropout.py
@@ -3,6 +3,7 @@ import numpy as np
 from math import ceil
 import torch.optim as optim
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.nn.utils.rnn import pack_padded_sequence
 from sklearn.metrics import accuracy_score
 
