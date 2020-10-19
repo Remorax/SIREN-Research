06361437da84141d303eb2ad43e082034245c3c2
Remorax <vivekbalasundaram@gmail.com>
2020-10-19 17:06:48 +0530

    Pushing results: Vivek

--- OntoEnricher/src/LSTM_instances.py
+++ OntoEnricher/src/LSTM_instances.py
@@ -153,7 +153,7 @@ def pad_edgecounts(edgecounts, max_paths):
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-num_epochs = 200
+num_epochs = 50
 batch_size = 32
 bidirectional = True
 
